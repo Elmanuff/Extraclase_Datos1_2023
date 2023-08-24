a2bfb27984d1af5eef6ceca829959e50ea497576
Andres Blanco Coto <andblanco@estudiantec.cr>
2023-08-23 21:04:04 -0600

    Se genero el codigo de ClienteController

--- src/main/java/com/cliente/ClienteController.java
+++ src/main/java/com/cliente/ClienteController.java
@@ -1,4 +1,31 @@
-package com.cliente;
+package com.Cliente;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 public class ClienteController {
+    @FXML
+    public TextField texto_mensaje_cliente;
+    @FXML
+    public TextArea panel_mensajes_cliente = new TextArea();
+
+    @FXML
+    public void recibirMensaje(String mensaje){
+        panel_mensajes_cliente.appendText("Servidor: " + mensaje + "\n");
+    }
+
+    @FXML
+    public void enviarMensaje(){
+        String mensaje = texto_mensaje_cliente.getText();
+        if (!mensaje.isEmpty()) {
+            panel_mensajes_cliente.appendText("Tu: " + mensaje + "\n");
+            MainCliente.cliente.enviar(mensaje);
+            texto_mensaje_cliente.clear();
+        }
+    }
+
+    public void limpiarPanel(){
+        panel_mensajes_cliente.clear();
+    }
 }
