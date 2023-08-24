7e464decd934fb5ee57085492c8a6058022167f3
Andres Blanco Coto <andblanco@estudiantec.cr>
2023-08-23 21:04:33 -0600

    Se genero el codigo de InicioClienteController

--- src/main/java/com/cliente/InicioClienteController.java
+++ src/main/java/com/cliente/InicioClienteController.java
@@ -1,4 +1,38 @@
-package com.cliente;
+package com.Cliente;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
 
 public class InicioClienteController {
+    @FXML
+    public TextField texto_ip;
+    @FXML
+    public TextField texto_puerto;
+    @FXML
+    public Label validacion_datos = new Label();
+
+    @FXML
+    public void conectar(){
+        if (!texto_puerto.getText().isEmpty() && !texto_ip.getText().isEmpty()) {
+            try {
+                MainCliente.cliente.ejecutar(texto_ip.getText(), Integer.parseInt(texto_puerto.getText()));
+                validacion_datos.setText("");
+                validacion_datos.setTextFill(Color.WHITE);
+
+            } catch (Exception e) {
+                validarDatos();
+                throw new RuntimeException(e);
+            }
+        }else {
+            validarDatos();
+        }
+    }
+
+    @FXML
+    public void validarDatos(){
+        validacion_datos.setText("Ip o puerto invalidos");
+        validacion_datos.setTextFill(Color.RED);
+    }
 }
