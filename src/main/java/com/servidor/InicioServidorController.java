44f9caaa256408cae04f6be4100346e86d38a008
Andres Blanco Coto <andblanco@estudiantec.cr>
2023-08-23 21:08:16 -0600

    Se genero el codigo de InicioServidorController

--- src/main/java/com/servidor/InicioServidorController.java
+++ src/main/java/com/servidor/InicioServidorController.java
@@ -1,4 +1,36 @@
-package com.servidor;
+package com.Servidor;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
 
 public class InicioServidorController {
+    @FXML
+    public TextField texto_puerto;
+    @FXML
+    public Label validacion_datos = new Label();
+
+    @FXML
+    public void conectar(){
+        if (!texto_puerto.getText().isEmpty()) {
+            try {
+                MainServidor.servidor.ejecutar(Integer.parseInt(texto_puerto.getText()));
+                validacion_datos.setText("Esperando conexion...");
+                validacion_datos.setTextFill(Color.GREEN);
+
+            } catch (Exception e) {
+                validarDatos();
+                throw new RuntimeException(e);
+            }
+        }else {
+            validarDatos();
+        }
+    }
+
+    @FXML
+    public void validarDatos(){
+        validacion_datos.setText("Puerto Invalido");
+        validacion_datos.setTextFill(Color.RED);
+    }
 }
